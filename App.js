cb1077accf68f56b95bedf4a0448ff0e231d0ddf
Inayath Ali <inayath5026@gmail.com>
2025-01-24 02:12:17 +0530

    Update restaurant data and map to Card

--- App.js
+++ App.js
@@ -1,86 +1,75 @@
-import React from "react"
+import React from "react";
 import ReactDOM from "react-dom/client";
+import {data} from "./data.js";
+
+console.log(data);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //Header Component
 let Header = () => {
-
-    return (
-        <>
-
-            <div className="header">
-                <div className="logo">
-                    <img src="https://png.pngtree.com/png-vector/20221231/ourmid/pngtree-food-delivery-logo-template-design-sign-menu-vector-png-image_43664212.jpg" />
-                </div>
-                <div className="nav-links">
-                    <ul>
-                        <li>About</li>
-                        <li>Contact</li>
-                        <li>Cart</li>
-                        <li>Help</li>
-                    </ul>
-                </div>
-
-            </div>
-
-        </>
-    )
-
-}
+  return (
+    <>
+      <div className="header">
+        <div className="logo">
+          <img src="https://png.pngtree.com/png-vector/20221231/ourmid/pngtree-food-delivery-logo-template-design-sign-menu-vector-png-image_43664212.jpg" />
+        </div>
+        <div className="nav-links">
+          <ul>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Cart</li>
+            <li>Help</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
 //Body Component
 let Body = () => {
+  return (
+    <>
+      <div className="Body">
+        <div className="search">
+          <input type="text" />
+          <button>Search</button>
+        </div>
+        <div className="card-container">
+          {
+            data.restaurants.map((restaurant) => (
+                <Card resData={restaurant} />
+              ))
+              
+          }
+        </div>
+      </div>
+    </>
+  );
+};
 
-    return (
-        <>
-        
-            <div className="Body">
+let Card = ({resData}) => {
 
-                <div className="search">
-
-                    <input type="text"/>
-                    <button>Search</button>
-
-                </div>
-                <div className="card-container">
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                </div>
-
-            </div>
-        
-        </>
-    );
-
-}
-
-let Card = () => {
-    return (
-        <>
-        
-            <div className="card">
-                <img src="https://www.cubesnjuliennes.com/wp-content/uploads/2020/07/Chicken-Biryani-Recipe.jpg"/>
-                <h4>Mehfil Restaurant</h4>
-                <h4>Biryani, North Indian, Asian</h4>
-                <h4>4.8 stars</h4>
-                <h4>26 mins</h4>
-            </div>
-
-        </>
-    );
-}
+  return (
+    <>
+      <div className="card">
+        <img src={resData.image} style={{color:"orange"}}/>
+        <h4>{resData.name}</h4>
+        <h4>{resData.cuisines.join(", ")}</h4>
+        <h4>{resData.rating} stars</h4>
+        <h4>{resData.delivery_time} mins</h4>
+      </div>
+    </>
+  );
+};
 
 let App = () => {
-    return (
-        <>
-            <Header />
-            <Body />
-
-        </>
-    );
-}
-
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
+};
 
 root.render(<App />);
---
+++ data.js
@@ -0,0 +1,86 @@
+ const data = {
+    "restaurants": [
+      {
+        "name": "The Spice Hub",
+        "cuisines": ["Indian", "Chinese", "Continental"],
+        "price_for_two": 500,
+        "rating": 4.5,
+        "delivery_time": "30 mins",
+        "image": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/pglsdoet4dzxo1cmmk6h"
+      },
+      {
+        "name": "Burger Delight",
+        "cuisines": ["American", "Fast Food"],
+        "price_for_two": 300,
+        "rating": 4.2,
+        "delivery_time": "25 mins",
+        "image": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/61bc2fd2226bb0cf7fb5f78fcdcdb0cb"
+      },
+      {
+        "name": "Green Leaf Cafe",
+        "cuisines": ["Healthy", "Salads", "Vegan"],
+        "price_for_two": 400,
+        "rating": 4.7,
+        "delivery_time": "35 mins",
+        "image": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/RX_THUMBNAIL/IMAGES/VENDOR/2025/1/17/941a68ee-1814-41ca-ba95-9b31b6248a71_251527.JPG"
+      },
+      {
+        "name": "Pasta Paradise",
+        "cuisines": ["Italian"],
+        "price_for_two": 600,
+        "rating": 4.3,
+        "delivery_time": "40 mins",
+        "image": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/1741de2656f13f7f12ae5b417e2e804a"
+      },
+      {
+        "name": "Tandoori Tales",
+        "cuisines": ["North Indian", "Mughlai"],
+        "price_for_two": 450,
+        "rating": 4.4,
+        "delivery_time": "30 mins",
+        "image": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/94022b3ff709110f2f624a9dbe08b948"
+      },
+      {
+        "name": "Sushi Express",
+        "cuisines": ["Japanese", "Asian"],
+        "price_for_two": 800,
+        "rating": 4.8,
+        "delivery_time": "45 mins",
+        "image": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/afad4b985a251d023db9e2d1dfff2b2d"
+      },
+      {
+        "name": "Biryani Bliss",
+        "cuisines": ["Biryani", "Hyderabadi"],
+        "price_for_two": 550,
+        "rating": 4.6,
+        "delivery_time": "35 mins",
+        "image": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/FOOD_CATALOG/IMAGES/CMS/2024/3/22/198c3d14-3ce8-4105-8280-21577c26e944_779c4353-2f85-4515-94d2-208e90b830eb.png"
+      },
+      {
+        "name": "Mexican Fiesta",
+        "cuisines": ["Mexican"],
+        "price_for_two": 500,
+        "rating": 4.3,
+        "delivery_time": "30 mins",
+        "image": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/740a92bd202ffd40813bc354b86005db"
+      },
+      {
+        "name": "Thai Treats",
+        "cuisines": ["Thai", "Asian"],
+        "price_for_two": 650,
+        "rating": 4.5,
+        "delivery_time": "40 mins",
+        "image": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/jyq3caes7wmvwdxco07a"
+      },
+      {
+        "name": "Cafe Mocha",
+        "cuisines": ["Cafe", "Desserts", "Beverages"],
+        "price_for_two": 350,
+        "rating": 4.1,
+        "delivery_time": "20 mins",
+        "image": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/ca9209da500a634765c58417ddd45dee"
+      }
+    ]
+  }  
+
+export {data};
