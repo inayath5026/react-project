08bd834eab941fc0b457c56c7aaa377245d66f56
Inayath Ali <inayath5026@gmail.com>
2025-01-28 11:01:15 +0530

    Cleaning up code & Updated search functionality

--- Components/Body.jsx
+++ Components/Body.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Card from "./Card";
 import Shimmer from "./Shimmer";
+import { handleSearch, topRated, fetchData } from "../helper";
 
 export default Body = () => {
   const [resList, setResList] = useState([]);
@@ -8,53 +9,31 @@ export default Body = () => {
   const [inputText, setInputText] = useState("");
 
   useEffect(() => {
-    fetchData();
+    fetchData(setResList, setFilteredList);
   }, []);
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch(
-        "https://www.swiggy.com/dapi/restaurants/search/v3?lat=17.406498&lng=78.47724389999999&str=North%20Indian&trackingId=5ca992b5-6660-8b62-879d-ea987c2c0f7b&submitAction=ENTER&queryUniqueId=ccdd32a2-b22f-eff8-a46b-4c7f75c572f3"
-      );
-      const jsonData = await response.json();
-      const restaurantCards =
-        jsonData.data.cards[1]?.groupedCard?.cardGroupMap?.RESTAURANT?.cards ||
-        [];
-      setResList(restaurantCards);
-      setFilteredList(restaurantCards);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  const topRated = () => {
-    setFilteredList(
-      filteredList.filter((restaurant) => {
-        const restaurantInfo = restaurant.card.card.info;
-        return restaurantInfo.avgRating > 4.5;
-      })
-    );
-  };
-
-  const handleSearch = () => {
-    const fL = resList.filter((res) =>
-      res.card.card.info.name.toLowerCase().includes(inputText.toLowerCase())
-    );
-    setFilteredList(fL);
-  };
 
   const handleInputText = (e) => {
     setInputText(e.target.value);
+    handleSearch(resList, setFilteredList, inputText);
   };
 
   return (
     <div className="Body">
       <div className="search">
         <input type="text" value={inputText} onChange={handleInputText} />
-        <button onClick={handleSearch}>Search</button>
+        <button
+          onClick={() => {
+            handleSearch(resList, setFilteredList, inputText);
+          }}
+        >
+          Search
+        </button>
       </div>
 
-      <button onClick={topRated} className="top-rated">
+      <button
+        onClick={() => topRated(filteredList, setFilteredList)}
+        className="top-rated"
+      >
         Top Rated
       </button>
 
---
+++ helper.js
@@ -0,0 +1,36 @@
+//Top Rated Filtering Function
+const topRated = (filteredList, setFilteredList) => {
+  setFilteredList(
+    filteredList.filter((restaurant) => {
+      const restaurantInfo = restaurant.card.card.info;
+      return restaurantInfo.avgRating > 4.5;
+    })
+  );
+};
+
+//Handling The Search Function
+const handleSearch = (resList, setFilteredList, inputText) => {
+  const fL = resList.filter((res) =>
+    res.card.card.info.name.toLowerCase().includes(inputText.toLowerCase())
+  );
+  setFilteredList(fL);
+};
+
+//Fetching Data From API
+const fetchData = async (setResList, setFilteredList) => {
+  try {
+    const response = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/search/v3?lat=17.406498&lng=78.47724389999999&str=North%20Indian&trackingId=5ca992b5-6660-8b62-879d-ea987c2c0f7b&submitAction=ENTER&queryUniqueId=ccdd32a2-b22f-eff8-a46b-4c7f75c572f3"
+    );
+    const jsonData = await response.json();
+    const restaurantCards =
+      jsonData.data.cards[1]?.groupedCard?.cardGroupMap?.RESTAURANT?.cards ||
+      [];
+    setResList(restaurantCards);
+    setFilteredList(restaurantCards);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+
+export { topRated, handleSearch, fetchData };
